0b14385a867339c3d23e99f212ab22c6a44bc658
Dusan Dimic <dusan.dimic@vegait.rs>
2025-01-06 13:02:30 +0100

    Initial Commit

---
+++ src/main/java/com/dreamteam/javafit/JavafitApplication.java
@@ -0,0 +1,13 @@
+package com.dreamteam.javafit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavafitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavafitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dreamteam/javafit/JavafitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamteam.javafit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavafitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
